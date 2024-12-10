8f12cbeb022a4cfa8b6f4b6a0e8c015d56f29afd
dytni <nikolajolesko391@gmail.com>
2024-12-10 09:35:18 +0300

    refactor

--- src/main/java/com/hatsukha/nikolai/client/utils/StyleUtils.java
+++ src/main/java/com/hatsukha/nikolai/client/utils/StyleUtils.java
@@ -2,7 +2,6 @@ package com.hatsukha.nikolai.client.utils;
 
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class StyleUtils {
